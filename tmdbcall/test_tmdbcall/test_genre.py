e87d6bf4fa783002102957a328746504c8b58c69
Tobias <spitzfaust@gmail.com>
2015-12-09 22:24:19 +0100

    Added test cases for the Job and Genre class

---
+++ tmdbcall/test_tmdbcall/test_genre.py
@@ -0,0 +1,26 @@
+"""This file holds the tests for the Genre class."""
+from django.test import TestCase
+
+from tmdbcall.genre import Genre
+
+
+class TestGenre(TestCase):
+    """
+    Testcases for the Genre class
+
+    Attributes:
+        test (Genre): Test instance of the Genre class
+    """
+    test = Genre()
+
+    def test_get_genres(self):
+        """
+        Test if the get_genres() method returns a
+        dictionary with the genres key.
+        """
+        result = self.test.get_genres()
+        if 'genres' in result:  # Check if the genres key exists in the dict
+            got_genres = True
+        else:
+            got_genres = False
+        self.assertTrue(got_genres)
