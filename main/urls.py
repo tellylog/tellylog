6e1bbeb94475bb738c4a3aa01b63489eb45e9005
Thomas Rauhofer <pommi15@hotmail.com>
2015-12-14 19:06:45 +0100

    changed the urls to lowercase

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.Main, name='main'),
-    url(r'^aboutUs/$', views.AboutUs, name='aboutUs'),
-    url(r'^logIn/$', views.logIn, name='logIn'),
+    url(r'^$', views.main, name='main'),
+    url(r'^aboutus/$', views.aboutus, name='aboutus'),
+    url(r'^login/$', views.login, name='login'),
 ]
