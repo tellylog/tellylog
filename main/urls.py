353ea8d3ac97913dde384e0a2467b76ccbb653c0
Thomas Rauhofer <pommi15@hotmail.com>
2015-12-15 11:55:24 +0100

    corrected urls and views
    fixed a few bugs in urls and views, added a new view

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-
+from main.views import MainView
 from . import views
 
-app_name = 'blog'
+app_name = 'main'
 urlpatterns = [
-    url(r'^$', views.main, name='main'),
+    # url(r'^$', views.main, name='main'),
+    url(r'^$', MainView.as_view(), name='main'),
     url(r'^aboutus/$', views.aboutus, name='aboutus'),
     url(r'^login/$', views.login, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,1 +1,12 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
+
+
+
+class MainView(TemplateView):
+    template_name = "main.html"
+    """
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        return context
+    """
