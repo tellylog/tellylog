0fa6e85045d3584e6621099ddfd1d6e710144dd8
Thomas Rauhofer <pommi15@hotmail.com>
2015-12-15 21:00:01 +0100

    deleted the commented urls as they are useless

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 app_name = 'main'
 urlpatterns = [
-    # url(r'^$', views.main, name='main'),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^about/$', views.AboutView.as_view(), name='about'),
-    # url(r'^login/$', views.login, name='login'),
 ]
