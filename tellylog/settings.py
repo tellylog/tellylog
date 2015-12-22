8a0d856dabd07d81810bf1e9daeb2e6b1f4a717c
Tobias <spitzfaust@gmail.com>
2015-12-23 00:51:44 +0100

    Reading and writing skills are sometimes essential.

--- tellylog/settings.py
+++ tellylog/settings.py
@@ -19,20 +19,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-if os.getenv('CI') is not None:
-    _API_KEY = os.environ['SECRET_KEY']
+if 'CI' in os.environ:
+    SECRET_KEY = os.environ['SECRET_KEY']
 else:
-    _API_KEY = env('SECRET_KEY')
+    SECRET_KEY = env('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if os.getenv('CI') is not None:
+if 'CI' in os.environ:
     DEBUG = False
 else:
-    _API_KEY = env('DEBUG')
+    DEBUG = env('DEBUG')
 
 
 ALLOWED_HOSTS = ['*', ]
--- tmdbcall/key.py
+++ tmdbcall/key.py
@@ -2,7 +2,7 @@
 import os
 from getenv import env
 
-if os.getenv('CI') is not None:
+if 'CI' in os.environ:
     _API_KEY = os.environ['TMDB_API_KEY']
 else:
     _API_KEY = env('TMDB_API_KEY')
