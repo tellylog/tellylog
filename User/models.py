ddd476cf17714e643606dff7b5f7efbed91723d4
Thomas Rauhofer <pommi15@hotmail.com>
2015-12-26 16:30:20 +0100

    more steps towards implementing the user sign up in the main page

--- User/models.py
+++ User/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __unicode__(self):
         return self.user.username
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from .forms import SignInForm
+from User.forms import UserForm
+from User.models import UserProfile
 
 
 class Index(FormView):
@@ -23,3 +25,4 @@ class SignIn(TemplateView):
 
 class SignUp(TemplateView):
     template_name = "main/sign_up.html"
+    user_forms = UserForm
