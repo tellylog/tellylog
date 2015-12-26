f60e6573e13854a2c3deaa0b4c472eeccf230aa3
Thomas Rauhofer <pommi15@hotmail.com>
2015-12-26 14:46:52 +0100

    added sign in form on main page

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,5 @@ from User.models import UserProfile
 
 class SignInForm(forms.Form):
     username = forms.CharField(max_length=128)
-    password = forms.CharField(widget=forms.HiddenInput(), initial=0)
+    password = forms.CharField(
+        max_length=128, widget=forms.HiddenInput(), initial=0)
