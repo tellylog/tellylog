f0be333a04a431b4d7eda22b78d9fd9b4dc98ed9
Thomas Rauhofer <pommi15@hotmail.com>
2015-12-28 16:38:31 +0100

    added error if email adress is already taken in User database

--- User/forms.py
+++ User/forms.py
@@ -9,3 +9,11 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(
+                email=email).exclude(username=username).count():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+        return email
