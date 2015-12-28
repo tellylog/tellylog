946015330b2a5e6ebc23d42cb23fe5a1e8fa4489
Thomas Rauhofer <pommi15@hotmail.com>
2015-12-28 18:37:06 +0100

    created a sign in on the main page

--- tellylog/settings.py
+++ tellylog/settings.py
@@ -117,7 +117,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
