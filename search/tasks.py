59427313e133f94c67606f0b46f59a21491a8d71
Tobias <spitzfaust@gmail.com>
2016-02-11 23:51:18 +0100

    Commented tv convert, search tasks and views. Optimised SearchView

--- search/tasks.py
+++ search/tasks.py
@@ -1,3 +1,4 @@
+"""Celery tasks to search for a series in tmdb"""
 import time
 from celery import shared_task, chain, chord
 from tv import convert
@@ -8,36 +9,79 @@ from tv.models import Series
 
 @shared_task()
 def search_ready(result):
+    """Is called at the end of the chord of tasks.
+
+    Args:
+        result (bool): Return values of a single chain of tasks
+
+    Returns:
+        bool: Return value of the task
+    """
     return result
 
 
 @shared_task()
 def dummy():
+    """Just returns True
+
+    Returns:
+        bool: Always True
+    """
     return True
 
 
 @shared_task()
-def search_online(query, task_id=None):
+def search_online(query):
+    """Searches tmdb for the query and starts celery tasks
+
+    Args:
+        query (str): Search query
+
+    Raises:
+        Series.UpdateMe: The series needs an update.
+
+    Returns:
+        object: Celery task
+        bool: False on failure or if no results where found
+    """
     tmdb_tv = tmdb.tv.TV()
+    # search task for the series
     api_series = tmdb_tv.search_for_series(query)
+    # wait for the task to finish or fail
     while api_series.status in states.UNREADY_STATES:
-        time.sleep(1)
+        time.sleep(0.1)
+    # check if the task exited successfull
     if api_series.status == states.SUCCESS:
+        # check if there are results
         if api_series.result and api_series.result['total_results'] > 0:
+            # list for the convert chained tasks
             to_convert = []
+            # go through every result of the search
             for series in api_series.result['results']:
                 try:
+                    # try to get the series from the database
                     obj = Series.objects.get(tmdb_id=series['id'])
+                    # retrieved series needs an update
                     if obj.update_needed():
+                        # raise an UpdateMe exception
                         raise Series.UpdateMe('update me please')
+                    # no update needed
                     else:
+                        # append the dummy task to the list
                         to_convert.append(dummy.s())
+                # catch the DoesNotExist error or the UpdateMe error
                 except (Series.DoesNotExist, Series.UpdateMe):
+                    # add a chain of tasks to the list
+                    # get the full series -> check genres -> check countrys
+                    # -> process full series
                     to_convert.append(chain(convert.get_full_series.s(series),
                                             convert.check_genres.s(),
                                             convert.check_countrys.s(),
                                             convert.process_full_series.s()))
+            # to_convert holds tasks
             if len(to_convert) > 0:
+                # make a chord out of the list of chains and call
+                # search_ready when all end
                 converter = chord(to_convert)(search_ready.s())
                 return converter
     return False
--- search/views.py
+++ search/views.py
@@ -1,6 +1,6 @@
 """This file holds the views of the search app."""
 from django.http import JsonResponse
-from django.views.generic import View, ListView
+from django.views.generic import View, TemplateView
 from django.core.urlresolvers import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from watson import search as watson
@@ -9,31 +9,54 @@ from tellylog.celery import app
 import search.tasks as tasks
 
 
-class SearchView(LoginRequiredMixin, ListView):
+class SearchView(LoginRequiredMixin, TemplateView):
+    """View for the search
+
+    Attributes:
+        http_method_names (list): Allowed http methodes, only get is allowed
+        query (str): Query
+        query_param (str): GET parameter for the query
+        task_id (str, bool): ID of the search task or False if no results
+        template_name (str): Name of the template to render
+    """
     http_method_names = ['get']
     query_param = "q"
     template_name = "search/search.html"
     query = ''
 
     def get_query_param(self):
+        """Get the query parameter
+
+        Returns:
+            str: Query parameter
+        """
         return self.query_param
 
     def get_query(self, request):
-        """Parses the query from the request."""
+        """Parses the query from the request.
+
+        Args:
+            request (object): HTTPRequest
+        """
         return request.GET.get(self.get_query_param(), "").strip()
 
-    def get_queryset(self):
-        self.query = self.get_query(self.request)
-        search_res = watson.search(self.query)
+    def start_search(self):
+        """Start tmdb search
 
+        Returns:
+            bool: False on failure or if nothing was found
+            str: Task ID of the search task
+        """
+        # start the search task
         search_task = tasks.search_online(query=self.query)
+        # search task equals to True
         if search_task:
-            self.task_id = search_task.task_id
+            # get task id of search task
+            task_id = search_task.task_id
         else:
-            self.task_id = False
-        search_res = False
-
-        return search_res
+            # set task id to false
+            task_id = False
+        return task_id
 
     def get_context_data(self, **kwargs):
         """
@@ -45,13 +68,20 @@ class SearchView(LoginRequiredMixin, ListView):
         Returns:
             dict: Context dictionary with all values.
         """
+        # get the query
+        self.query = self.get_query(self.request)
+        # set the task id and start the search
+        self.task_id = self.start_search()
+        # get the existing values of the context array
         context = super(SearchView, self).get_context_data(**kwargs)
         context['query'] = self.query
         context['task_id'] = (self.task_id if
                               self.task_id is not None else False)
+        # url to make ajax call to SearchStatus View
         status_url = reverse('search:status')
         status_url = self.request.build_absolute_uri(status_url)
         context['status_url'] = status_url
+        # url to make ajax call to SearchResult View
         result_url = reverse('search:result')
         result_url = self.request.build_absolute_uri(result_url)
         context['result_url'] = result_url
@@ -59,37 +89,79 @@ class SearchView(LoginRequiredMixin, ListView):
 
 
 class SearchStatus(LoginRequiredMixin, View):
+    """Returns a JSON response with the status of the task
+
+    Attributes:
+        http_method_names (list): Allowed http methodes, only post is allowed
+    """
+    http_method_names = ['post']
+
     def post(self, request):
+        """Return a JSON response with the status of the task
+
+        Args:
+            request (object): HTTPRequest object
+
+        Returns:
+            object: JsonResponse with the status of the task
+        """
+        # get the task id from the request
         task_id = request.POST['task_id']
+        # get the task via id
         result = AsyncResult(task_id, app=app)
         response = JsonResponse({'status': result.status})
         return response
 
 
 class SearchResult(LoginRequiredMixin, View):
-    """docstring for SearchResult"""
+    """Returns a Json response of a search query
+
+    Attributes:
+        http_method_names (list): Allowed http methodes, only post is allowed
+    """
+    http_method_names = ['post']
+
     def post(self, request):
+        """Returns a Json response of a search query
+
+        Args:
+            request (object): HTTPRequest
+
+        Returns:
+            object: JsonResponse
+        """
+        # get query out of request
         query = request.POST['query']
-        # TODO JSON Serialize the Search results
+        # search for the query in the database
         search_res = watson.search(query)
+        # no search results
         if search_res.count() < 1:
             response = JsonResponse({'search_res': []})
+        # search results
         else:
+            # list of the search results
             search_res_list = []
+            # go through all search results and add them to the list
             for sres in search_res:
+                # set the sres to the real result
                 sres = sres.object
+                # result dict
                 res = {}
+                # set the values of the res to the sres
                 res['name'] = sres.name
                 res['overview'] = sres.overview
                 res['year'] = (sres.first_air_date.year if
                                sres.first_air_date else None)
                 res['genres'] = sres.get_genre_list()
+                # try to get the poster url
                 try:
                     res['poster'] = sres.poster_large.url
+                # no poster is present
                 except ValueError:
                     res['poster'] = False
+                # url of the series
                 res['url'] = sres.get_absolute_url()
+                # add the result dict to the search result list
                 search_res_list.append(res)
-
             response = JsonResponse({'search_res': search_res_list})
         return response
